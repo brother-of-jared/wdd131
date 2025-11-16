376ff9b03adf8c765a0d424a4cb6d63691eaff1b
brother-of-jared <mahonribalajadia@gmail.com>
2025-11-16 10:10:09 +0800

    updated the javascript for places.html

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
+// For footer
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+
+currentyear.innerHTML = `&copy; <span id="currentyear">${today.getFullYear()}</span>`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+// For mobile nav
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector("nav");
 
